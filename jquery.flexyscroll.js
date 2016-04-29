4a3498c1f561fc7b63876f1a4215ae8b192b54a9
Londomloto <roso.sasongko@gmail.com>
2016-04-29 09:54:59 +0700

    Create jquery.flexyscroll.js

---
+++ jquery.flexyscroll.js
@@ -0,0 +1,48 @@
+
+
+(function($){
+
+var Flexyscroll = function(element, options) {
+  this.element = $(element);
+  this.init(options);
+};
+
+Flexyscroll.defaults = {
+  
+};
+
+Flexyscroll.prototype = {
+  init: function(options) {
+    this.options = $.extend(true, {}, Flexyscroll.defaults, options || {});
+  },
+  render: function(data) {
+    
+  }
+};
+
+$.fn.flexyscroll = function() {
+  var 
+    args = $.makeArray(arguments),
+    init = $.type(args[0]) !== 'string';
+    
+  var func, list;
+  
+  list = this.each(function(){
+    var obj = $.data(this, 'flexyscroll);
+    if ( ! obj) {
+      $.data(this, 'flexyscroll', (obj = new Flexyscroll(this, args[0])));
+    }
+    if ( ! init) {
+      var method = args.shift();
+      if (obj[method] !== undefined) {
+        func = obj[method].apply(method, args);
+      } else {
+        throw new Error('Flexyscroll: method `' + method + '` doesn\'t exists'); 
+      }
+    }
+  });
+  
+  return init ? list : func;
+};
+
+}(jQuery))
